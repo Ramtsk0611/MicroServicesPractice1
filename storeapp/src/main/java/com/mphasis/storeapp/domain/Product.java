2b69de31cafeffee6972051f246540589d1807f0
RamTsk <ramtsk24685@gmail.com>
2023-04-15 09:52:08 +0530

    Added Store Config Server

--- storeapp/src/main/java/com/mphasis/storeapp/domain/Product.java
+++ storeapp/src/main/java/com/mphasis/storeapp/domain/Product.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class Product {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	@Column(name = "product_price")
